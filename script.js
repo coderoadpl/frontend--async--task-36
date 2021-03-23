8cdce24f24cafee67d3c034bd296abe4b77b3a15
Mateusz Choma <kontakt@coderoad.pl>
2021-03-23 20:24:05 +0100

    Promise all functional approach

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-const fakeFetch1 = () => new Promise((resolve) => setTimeout(() => resolve('data1'), 3000))
-const fakeFetch2 = () => new Promise((resolve, reject) => setTimeout(() => reject('data2'), 2000))
-const fakeFetch3 = () => new Promise((resolve) => setTimeout(() => resolve('data3'), 1000))
+const fakeFetch1 = () => new Promise((resolve) => setTimeout(() => resolve('data1'), 1000))
+const fakeFetch2 = () => new Promise((resolve) => setTimeout(() => resolve('data2'), 2000))
+const fakeFetch3 = () => new Promise((resolve) => setTimeout(() => resolve('data3'), 3000))
 
-let resolvedPromises = 0
-let promisesValues = []
+const promiseAll = (promises) => {
+    return new Promise((resolve, reject) => {
+        let resolvedPromises = 0
+        let promisesValues = []
 
-const onResolveAll = (data) => {
-    if (resolvedPromises === 3) {
-        console.log('all resolved', promisesValues)
-    }
+        const onResolveAll = (data) => {
+            if (resolvedPromises === 3) {
+                resolve(promisesValues)
+            }
+        }
+        const onReject = (error) => {
+            reject(error)
+        }
+
+        promises.forEach((promise, index) => {
+            promise
+                .then((data) => {
+                    resolvedPromises++
+                    promisesValues[index] = data
+                    return data
+                })
+                .then(onResolveAll)
+                .catch(onReject)
+        })
+    })
 }
-const onReject = (error) => {
-    console.log('all rejected', error)
-}
 
-fakeFetch1()
-    .then((data) => {
-        resolvedPromises++
-        promisesValues[0] = data
-        return data
-    })
-    .then(onResolveAll)
-    .catch(onReject)
+const promise = promiseAll([
+    fakeFetch1(),
+    fakeFetch2(),
+    fakeFetch3(),
+])
 
-fakeFetch2()
-    .then((data) => {
-        resolvedPromises++
-        promisesValues[1] = data
-        return data
-    })
-    .then(onResolveAll)
-    .catch(onReject)
-
-fakeFetch3()
-    .then((data) => {
-        resolvedPromises++
-        promisesValues[2] = data
-        return data
-    })
-    .then(onResolveAll)
-    .catch(onReject)
+promise
+    .then((data) => console.log('promise all', data))
+    .catch((error) => console.log('promise all rejected', error))
